71fc9fd81bb5fab2599018f7806245f3929262a2
James Devereux <devereuxjj@gmail.com>
2022-10-07 10:54:52 +0900

    add devise and teacher student models

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def devise_parameter_sanitizer
+    if resource_class == Teacher
+      Teacher::ParameterSanitizer.new(Teacher, :teacher, params)
+    elsif resource_class == Student
+      Student::ParameterSanitizer.new(Student, :student, params)
+    else
+      super # Use the default one
+    end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :first_name, :last_name, ])
+  end
+end
+
+
+class Teacher::ParameterSanitizer < Devise::ParameterSanitizer
+  def initialize(*)
+    super
+  end
+end
+
+class Student::ParameterSanitizer < Devise::ParameterSanitizer
+  def initialize(*)
+    super
+    permit(:sign_up, keys: [:student_number])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :students
+  devise_for :teachers
   root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
