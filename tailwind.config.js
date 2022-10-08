43eb1e31a76b253986a99f5039c35a383f7227ed
James Devereux <devereuxjj@gmail.com>
2022-10-08 09:28:24 +0900

    setup tailwind ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+
 module.exports = {
   content: [
     "./app/helpers/**/*.rb",
@@ -6,7 +10,11 @@ module.exports = {
     "./app/views/**/*",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
