761b7baaac731ee3de1c0c77bbc72ce2d82a4d63
James Devereux <devereuxjj@gmail.com>
2022-10-17 11:00:20 +0900

    remove session store domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-Zotic::Application.config.session_store :cookie_store,
-                                        key: "_app_session",
-                                        domain:
-                                          "https://hammerhead-app-yckas.ondigitalocean.app"
+# Zotic::Application.config.session_store :cookie_store,
+#                                         key: "_app_session",
+#                                         domain:
+#                                           "https://hammerhead-app-yckas.ondigitalocean.app"
