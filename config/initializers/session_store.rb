63234302270bb464b2112fe3b5e142d9d7182c00
James Devereux <devereuxjj@gmail.com>
2022-10-17 11:13:42 +0900

    add session store

--- config/initializers/session_store.rb
+++
@@ -1,4 +0,0 @@
-# Zotic::Application.config.session_store :cookie_store,
-#                                         key: "_app_session",
-#                                         domain:
-#                                           "https://hammerhead-app-yckas.ondigitalocean.app"
