b028e3a353789e73c41ac70ea0a2d000d4aa2c1f
James Devereux <devereuxjj@gmail.com>
2022-10-17 10:48:22 +0900

    add session store domain

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,7 @@
 class Student < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable,
-         :registerable,
-         :recoverable,
-         :rememberable,
-         :validatable
+  # devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :enrollments, dependent: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,36 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Teacher.destroy_all
 Student.destroy_all
 Course.destroy_all
 Enrollment.destroy_all
 
-teacher = Teacher.create!(email: "freewaytofluency@gmail.com", password: 'creamtea', first_name: "James", last_name: "Devereux")
+teacher = Teacher.find_by(email: "freewaytofluency@gmail.com")
 
-student = Student.create!(email: 'student@gmail.com', password: 'student', first_name: 'Mahiro', last_name: 'Suzuki', student_number: '221231231')
-student_two = Student.create!(email: 'student2@gmail.com', password: 'student', first_name: 'Honami', last_name: 'Suzuki', student_number: '221231231')
-student_three = student = Student.create!(email: 'student3@gmail.com', password: 'student', first_name: 'Rin', last_name: 'Suzuki', student_number: '221231231')
+student =
+  Student.create!(
+    email: "student@gmail.com",
+    first_name: "Mahiro",
+    last_name: "Suzuki",
+    student_number: "221231231",
+  )
+student_two =
+  Student.create!(
+    email: "student2@gmail.com",
+    first_name: "Honami",
+    last_name: "Suzuki",
+    student_number: "221231231",
+  )
+student_three =
+  student =
+    Student.create!(
+      email: "student3@gmail.com",
+      first_name: "Rin",
+      last_name: "Suzuki",
+      student_number: "221231231",
+    )
 
-course = Course.create!(name: 'Academic English', teacher: teacher)
+course = Course.create!(name: "Academic English", teacher: teacher)
 
 enrollment1 = Enrollment.create!(course: course, student: student)
 enrollment2 = Enrollment.create!(course: course, student: student_two)
